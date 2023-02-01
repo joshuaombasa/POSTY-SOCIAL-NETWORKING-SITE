1f74c11821939f4fe332e48270b97eacb6f40bd4
Joshua Ombasa <joshuaombasa@gmail.com>
2023-02-01 22:10:36 +0300

    added authorization

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 import express from "express"
-import myslq from "mysql"
+import mysql from "mysql"
 
 const app = express();
+
+const connection = mysql.createConnection( {
+    host: 'localhost',
+    user:'root',
+    password: '',
+    database: 'posty'
+}) 
 
 app.set('view engine', 'ejs')
 
@@ -47,12 +54,46 @@ app.get('/signup', (req, res) => {
 // submit signup form
 app.post('/signup', (req, res) => {
     const user = {
-        fulname: req.body.fullname,
+        fullname: req.body.fullname,
         email: req.body.email,
         password: req.body.password,
         confirmPassword: req.body.confirmPassword
     }
-    console.log(user)
+    if (user.password === user.confirmPassword) {
+        // check if user exists
+
+        let sql = 'SELECT * FROM users WHERE email = ?'
+        connection.query(
+            sql,
+            [user.email],
+            (error, results) => {
+                if (results.length > 0) {
+                    // user exists
+                    let error = true
+                    let message = 'Accout already exists with the email provided'
+
+                    res.render('signup', {user, error, message})
+                } else {
+                    // create user
+                    let sql = 'INSERT INTO users (username, email, password) VALUES (?, ?, ?)'
+                    connection.query(
+                        sql,
+                        [user.fullname, user.email, user.password],
+                        (error, results) => {
+                            console.log('user successfully created')
+                        }
+                    )
+                    
+                }
+            }
+        )
+
+    } else {
+        // passwords do not match
+        let error = true
+        let message = 'Passwords Mismatch'
+        res.render('signup', {user, error, message})
+    }
 })
 
 
